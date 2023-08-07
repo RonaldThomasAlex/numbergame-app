32b9c10aa6e049d95cf04dbbdc55ad595d0c263e
rthomas-unravel <rthomas@unraveldata.com>
2023-08-07 21:14:06 +0530

    cascading styles

--- components/ui/InstructionText.js
+++ components/ui/InstructionText.js
@@ -2,14 +2,14 @@ import { Text, StyleSheet } from "react-native";
 
 import Colors from "../../constants/color";
 
-function InstructionText({ children }) {
-  return <Text style={styles.instructionTex}>{children}</Text>;
+function InstructionText({ children, style }) {
+  return <Text style={[styles.instructionText, style]}>{children}</Text>;
 }
 
 export default InstructionText;
 
 const styles = StyleSheet.create({
-  instructionTex: {
+  instructionText: {
     color: Colors.accent500,
     fontSize: 24
   }
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { View, Text, StyleSheet, Alert } from "react-native";
+import { View, StyleSheet, Alert } from "react-native";
 
 import Title from "../components/ui/Title";
 import NumberContainer from "../components/game/NumberContainer";
@@ -59,15 +59,21 @@ function GameScreen({ userNumber, onGameOver }) {
 
       <Card>
         <View>
-          <InstructionText>Higher or lower ?</InstructionText>
+          <InstructionText style={styles.instructionText}>
+            Higher or lower ?
+          </InstructionText>
 
-          <View>
-            <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
-              -
-            </PrimaryButton>
-            <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
-              +
-            </PrimaryButton>
+          <View style={styles.buttonsContainer}>
+            <View style={styles.buttonContainer}>
+              <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
+                -
+              </PrimaryButton>
+            </View>
+            <View style={styles.buttonContainer}>
+              <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
+                +
+              </PrimaryButton>
+            </View>
           </View>
         </View>
       </Card>
@@ -81,6 +87,15 @@ export default GameScreen;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    padding: 54
+    padding: 24
+  },
+  buttonsContainer: {
+    flexDirection: "row"
+  },
+  buttonContainer: {
+    flex: 1
+  },
+  instructionText: {
+    marginBottom: 12
   }
 });
